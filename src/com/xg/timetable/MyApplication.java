6e6affdfe2a2e24f212b417f96ceee16a25c4f33
xgstlye <ywg910416@gmail.com>
2014-04-10 17:19:49 +0800

    第二次修改

---
+++ src/com/xg/timetable/MyApplication.java
@@ -0,0 +1,41 @@
+package com.xg.timetable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Application;
+
+public class MyApplication extends Application {
+	private List<Activity> activityList = new LinkedList<Activity>();
+	private static MyApplication instance;
+
+	private MyApplication() {
+	}
+
+	public static MyApplication getInstance() {
+		// TODO Auto-generated method stub
+		if (instance == null)
+			instance = new MyApplication();
+
+		return instance;
+	}
+
+	public void addActivity(Activity activity) {
+		activityList.add(activity);
+	}
+
+	public void exitApp() {
+		for (Activity activity : activityList) {
+			if (activity != null)
+				activity.finish();
+		}
+		System.exit(0);
+	}
+
+	public void onLowMemory() {
+		super.onLowMemory();
+		System.gc();
+	}
+
+}
