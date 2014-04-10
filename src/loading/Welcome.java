7272ff9b4b0883b92b8a968f68336af1cf533c6a
xgstlye <ywg910416@gmail.com>
2014-04-10 11:51:23 +0800

    College
    大学校园应用。
    
    第一次修改

---
+++ src/loading/Welcome.java
@@ -0,0 +1,31 @@
+package loading;
+
+import com.xg.mycollege.MainActivity;
+
+
+import com.xg.mycollege.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class Welcome extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	   setContentView(R.layout.welcome);
+	
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				Intent intent = new Intent();
+				intent.setClass(Welcome.this, MainActivity.class);
+				startActivity(intent);
+				Welcome.this.finish();
+			}
+
+		}, 5000);
+
+	}
+}
